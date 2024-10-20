7d6cd24ec2dd713f5f98287a27bc32729b256bce
Nico Jensch <root@dr460nf1r3.org>
2024-10-20 18:26:06 +0200

    chore: fix eslint config; return to prettier formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import nx from '@nx/eslint-plugin';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+})
+
+export default [
+    {
+        ignores: ["**/*", "**/node_modules"],
+    },
+    {
+        plugins: {
+            "@nx": nx,
+        },
+    },
+    {
+        files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+
+        rules: {
+            "@nx/enforce-module-boundaries": [
+                "error",
+                {
+                    enforceBuildableLibDependency: true,
+                    allow: [],
+
+                    depConstraints: [
+                        {
+                            sourceTag: "*",
+                            onlyDependOnLibsWithTags: ["*"],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    ...compat.extends("plugin:@nx/typescript").map((config) => ({
+        ...config,
+        files: ["**/*.ts", "**/*.tsx"],
+    })),
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        rules: {},
+    },
+    ...compat.extends("plugin:@nx/javascript").map((config) => ({
+        ...config,
+        files: ["**/*.js", "**/*.jsx"],
+    })),
+    {
+        files: ["**/*.js", "**/*.jsx"],
+        rules: {},
+    },
+]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,6 @@
-import { Route } from "@angular/router"
-import { generateRouterLink } from "./functions"
-import { HomeComponent } from "./home/home.component"
-import { RedirectGuard } from "./redirect/redirect.guard"
-import { RedirectorComponent } from "./redirector/redirector.component"
-import { SettingsComponent } from "./settings/settings.component"
-import { ContactLinks, MenuBarItems, ServiceLinks } from "./types"
-
-function generateExternalLinks(linkMap: ContactLinks | MenuBarItems | ServiceLinks): Route[] {
-    return Object.values(linkMap).map((key) => {
-        return {
-            path: generateRouterLink(key.title),
-            canActivate: [RedirectGuard],
-            component: RedirectorComponent,
-            data: {
-                externalUrl: key.link ? key.link : key.url,
-            },
-        }
-    })
-}
+import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const appRoutes: Route[] = [
     {
