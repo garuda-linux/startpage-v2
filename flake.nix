e97dd877bc80e755c43c631edd602c7de00068f6
Nico Jensch <root@dr460nf1r3.org>
2025-05-24 16:34:36 +0200

    chore(deps): update, support darwin devshells

--- flake.nix
+++ flake.nix
@@ -111,7 +111,7 @@
       imports = [
         inputs.pre-commit-hooks.flakeModule
       ];
-      systems = ["x86_64-linux" "aarch64-linux"];
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
       inherit perSystem;
     };
 }
